a4c5f0f56e7b3a926e44380d684b8d98ad40efde
ZhenyaZhak <zhenia.zhak@gmail.com>
2018-08-05 22:31:50 +0300

    Add basic validations for Event model

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,6 @@
 class Event < ApplicationRecord
+	validates :title, :place_title, :place_cord, :description, :start_at, :end_at, presence: true
+	validates :title, length: { in: 6..20 }
+	validates :place_title, length: { maximum: 30 }
+	validates :description, length: { minimum: 30 }
 end
