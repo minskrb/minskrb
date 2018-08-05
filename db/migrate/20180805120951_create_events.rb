1ea2de1a95375ec8043820fcc77125118769b3b8
ZhenyaZhak <zhenia.zhak@gmail.com>
2018-08-05 15:17:25 +0300

    Generate model Event with basic attributes

---
+++ db/migrate/20180805120951_create_events.rb
@@ -0,0 +1,14 @@
+class CreateEvents < ActiveRecord::Migration[5.2]
+  def change
+    create_table :events do |t|
+      t.string :title
+      t.string :place_title
+      t.string :place_cord
+      t.text :description
+      t.datetime :start_at
+      t.datetime :end_at
+
+      t.timestamps
+    end
+  end
+end
