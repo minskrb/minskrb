4ad65c058d8e846004bfec671faf751247b175be
ZhenyaZhak <zhenia.zhak@gmail.com>
2018-08-06 14:47:54 +0300

    Some changes in start_at, end_at validations

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,13 +3,5 @@ class Event < ApplicationRecord
   validates :title, length: { in: 6..20 }
   validates :place_title, length: { maximum: 30 }
   validates :description, length: { minimum: 30 }
-  validate :end_after_start
-
-  private
-
-  def end_after_start
-    return if end_at.blank? || start_at.blank?
-
-    errors.add(:end_date, 'must be after the start date') if end_at < start_at
-  end
+  validates_with StartEndTimeValidator
 end
