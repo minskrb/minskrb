6ae6a1734949de6a39669a120df47db35f81766d
ZhenyaZhak <zhenia.zhak@gmail.com>
2018-08-06 23:17:37 +0300

    Divide validations into group

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,26 +1,40 @@
 require 'rails_helper'
 
 RSpec.describe Event, type: :model do
-  %i[title place_title place_cord description start_at end_at].each do |attribute|
-    it "is valid when #{attribute} presence" do
-      should validate_presence_of(attribute)
+  describe 'Validations' do
+    describe '#title' do
+      it { should validate_presence_of(:title) }
+
+      it { should validate_length_of(:title).is_at_least(6).is_at_most(20) }
     end
-  end
 
-  it 'is valid when the length of :title is more than 6 and less than 20' do
-    should validate_length_of(:title).is_at_least(6).is_at_most(20)
-  end
+    describe '#place_title' do
+      it { should validate_presence_of(:place_title) }
 
-  it 'is valid when the length of :title is less than 30' do
-    should validate_length_of(:place_title).is_at_most(30)
-  end
+      it { should validate_length_of(:place_title).is_at_most(30) }
+    end
 
-  it 'is valid when the length of :title is more than 30' do
-    should validate_length_of(:description).is_at_least(30)
-  end
+    describe '#place_cord' do
+      it { should validate_presence_of(:place_cord) }
+    end
 
-  it 'is not valid when the start time is later than the end time' do
-    event = build(:event, end_at: 1.week.ago)
-    expect(event).to_not be_valid
+    describe '#description' do
+      it { should validate_presence_of(:description) }
+
+      it { should validate_length_of(:description).is_at_least(30) }
+    end
+
+    describe '#start_at' do
+      it { should validate_presence_of(:start_at) }
+    end
+
+    describe '#end_at' do
+      it { should validate_presence_of(:end_at) }
+
+      it 'is not valid when the start time is later than the end time' do
+        event = build(:event, end_at: 1.week.ago)
+        expect(event).to_not be_valid
+      end
+    end
   end
 end
