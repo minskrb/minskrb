9a2e1adb43e3fe1a0ec372b24d5956a3e9107f39
ZhenyaZhak <zhenia.zhak@gmail.com>
2018-08-06 09:53:12 +0300

    Add tests for Event validations

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,15 @@
 class Event < ApplicationRecord
-	validates :title, :place_title, :place_cord, :description, :start_at, :end_at, presence: true
-	validates :title, length: { in: 6..20 }
-	validates :place_title, length: { maximum: 30 }
-	validates :description, length: { minimum: 30 }
+  validates :title, :place_title, :place_cord, :description, :start_at, :end_at, presence: true
+  validates :title, length: { in: 6..20 }
+  validates :place_title, length: { maximum: 30 }
+  validates :description, length: { minimum: 30 }
+  validate :end_after_start
+
+  private
+
+  def end_after_start
+    return if end_at.blank? || start_at.blank?
+
+    errors.add(:end_date, 'must be after the start date') if end_at < start_at
+  end
 end
--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,5 +1,71 @@
 require 'rails_helper'
 
 RSpec.describe Event, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:event) {
+    Event.new(title: 'Minsk.rb best',
+              place_title: 'Eventspace.by',
+              place_cord: 'Oktyabrskaya str. 16a, Minsk',
+              description: 'Fixie tote bag ethnic keytar. Neutra vinyl American...',
+              start_at: Time.now,
+              end_at: Time.now + 1.week)
+  }
+
+  it 'is valid with valid attributes' do
+    expect(event).to be_valid
+  end
+
+  it 'is not valid without a title' do
+    event.title = nil
+    expect(event).to_not be_valid
+  end
+
+  it 'is not valid when the length of :title is less than 6' do
+    event.title = 'Minsk'
+    expect(event).to_not be_valid
+  end
+
+  it 'is not valid when the length is more than 20' do
+    event.title = 'We are Minsk.rb(EEE BOY)'
+    expect(event).to_not be_valid
+  end
+
+  it 'is not valid without a place_title' do
+    event.place_title = nil
+    expect(event).to_not be_valid
+  end
+
+  it 'is not valid when the length of :place_title is more than 30' do
+    event.place_title = 'Eventspce.by it`s in Belarus, Minsk'
+    expect(event).to_not be_valid
+  end
+
+  it 'is not valid without a place_cord' do
+    event.place_cord = nil
+    expect(event).to_not be_valid
+  end
+
+  it 'is not valid without a description' do
+    event.description = nil
+    expect(event).to_not be_valid
+  end
+
+  it 'is not valid when the length of :description is less than 30' do
+    event.description = 'Some text in description'
+    expect(event).to_not be_valid
+  end
+
+  it 'is not valid without a start_at' do
+    event.start_at = nil
+    expect(event).to_not be_valid
+  end
+
+  it 'is not valid without a end_at' do
+    event.end_at = nil
+    expect(event).to_not be_valid
+  end
+
+  it 'is not valid when the start time is later than the end time' do
+    event.end_at = Time.now - 1.week
+    expect(event).to_not be_valid
+  end
 end
