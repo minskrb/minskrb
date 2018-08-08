811a5433310efdb778b270cc27646d96d60c6f46
ZhenyaZhak <zhenia.zhak@gmail.com>
2018-08-08 10:08:22 +0300

    Add panel admin without autorisation

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
   resources :events, only: [:index, :show]
   root 'events#index'
+
+  namespace :admin do
+    resources :events
+
+    root to: "events#index"
+  end
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
