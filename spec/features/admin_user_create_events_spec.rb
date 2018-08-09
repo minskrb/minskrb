e55ec7135bb03dce3da31c7cc9586dc3f9e59c59
ZhenyaZhak <zhenia.zhak@gmail.com>
2018-08-09 23:13:40 +0300

    Fix facade of spec

--- spec/features/admin_user_create_events_spec.rb
+++ spec/features/admin_user_create_events_spec.rb
@@ -5,30 +5,34 @@ RSpec.describe 'Admin panel: Events creation', type: :system do
   let(:admin_user) { build_stubbed(:admin_user) }
 
   before :each do
-    @event_params ||= attributes_for(:event)
+    sign_in_to_admin_panel(email: admin_user.email, password: admin_user.password)
   end
 
-  scenario 'have an ability to go to Events creation page' do
-    sign_in_to_admin_panel(email: admin_user.email, password: admin_user.password)
-    click_link 'New event'
+  context 'when submitting event form with valid params' do
+    let(:event_params) { attributes_for(:event) }
 
-    expect(page).to have_content 'New Events'
+    scenario 'go to Events creation page' do
+      click_link 'New event'
+
+      expect(page).to have_content 'New Events'
+    end
+
+    scenario 'event created successful' do
+      click_link 'New event'
+      fill_event_form(event_params)
+
+      expect(page).to have_content 'Event was successfully created.'
+    end
   end
 
-  scenario 'correctly filled out the Event form' do
-    sign_in_to_admin_panel(email: admin_user.email, password: admin_user.password)
-    click_link 'New event'
-    fill_event_form(@event_params)
+  context 'when submitting event form with invalid params' do
+    let(:event_params) { attributes_for(:event, end_at: 1.week.ago) }
 
-    expect(page).to have_content 'Event was successfully created.'
-  end
+    scenario 'event don`t created' do
+      click_link 'New event'
+      fill_event_form(event_params)
 
-  scenario 'incorrectly filled out the Event form' do
-    sign_in_to_admin_panel(email: admin_user.email, password: admin_user.password)
-    click_link 'New event'
-    @event_params[:end_at] = 1.week.ago
-    fill_event_form(@event_params)
-
-    expect(page).to have_content 'End at must be after the start date'
+      expect(page).to have_content 'End at must be after the start date'
+    end
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -21,7 +21,6 @@ RSpec.configure do |config|
   config.before(:each, type: :system, js: true) do
     driven_by :selenium_chrome_headless
   end
-  #Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
