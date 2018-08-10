6bc6f58d07e1c74ee948d32cf85ad77f73e95897
ZhenyaZhak <zhenia.zhak@gmail.com>
2018-08-10 11:41:57 +0300

    Fix in scenario description

--- spec/features/admin_user_create_events_spec.rb
+++ spec/features/admin_user_create_events_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe 'Admin panel: Events creation', type: :system do
+RSpec.describe 'Admin panel:', type: :system do
   include Features::AdminUserHelpers
   let(:admin_user) { build_stubbed(:admin_user) }
 
@@ -8,31 +8,45 @@ RSpec.describe 'Admin panel: Events creation', type: :system do
     sign_in_to_admin_panel(email: admin_user.email, password: admin_user.password)
   end
 
-  context 'when submitting event form with valid params' do
-    let(:event_params) { attributes_for(:event) }
-
-    scenario 'go to Events creation page' do
-      click_link 'New event'
-
-      expect(page).to have_content 'New Events'
-    end
-
-    scenario 'event created successful' do
-      click_link 'New event'
-      fill_event_form(event_params)
-
-      expect(page).to have_content 'Event was successfully created.'
+  context 'on the admin homepage' do
+    context 'has an ability to go to events index page' do
+      scenario 'sees successful notification' do
+        expect(page).to have_content 'Signed in successfully.'
+      end
     end
   end
 
-  context 'when submitting event form with invalid params' do
-    let(:event_params) { attributes_for(:event, end_at: 1.week.ago) }
+  context 'on the events index page' do
+    context 'has an ability to go to events new page' do
+      scenario 'go to Events creation page' do
+        click_link 'New event'
 
-    scenario 'event don`t created' do
-      click_link 'New event'
-      fill_event_form(event_params)
+        expect(page).to have_content 'New Events'
+      end
+    end
+  end
 
-      expect(page).to have_content 'End at must be after the start date'
+  context 'on the events new page' do
+    context 'when submitting event form with valid params' do
+      let(:event_params) { attributes_for(:event) }
+
+      scenario 'sees successful notification' do
+        click_link 'New event'
+        fill_event_form(event_params)
+
+        expect(page).to have_content 'Event was successfully created.'
+      end
+    end
+
+    context 'when submitting event form with invalid params' do
+      let(:event_params) { attributes_for(:event, end_at: 1.week.ago) }
+
+      scenario 'sees not successful notification' do
+        click_link 'New event'
+        fill_event_form(event_params)
+
+        expect(page).to have_content 'End at must be after the start date'
+      end
     end
   end
 end
