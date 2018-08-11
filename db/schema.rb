db433bbd902502db9eabae89fdcb264a351f9c7b
ZhenyaZhak <zhenia.zhak@gmail.com>
2018-08-11 20:36:52 +0300

    Implement migration for create event_items table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_08_101656) do
+ActiveRecord::Schema.define(version: 2018_08_11_172853) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,17 @@ ActiveRecord::Schema.define(version: 2018_08_08_101656) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_admin_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
+  end
+
+  create_table "event_items", force: :cascade do |t|
+    t.string "title"
+    t.integer "item_type"
+    t.text "description"
+    t.string "speaker_name"
+    t.datetime "start_at"
+    t.datetime "end_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "events", force: :cascade do |t|
