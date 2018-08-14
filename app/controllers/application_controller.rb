b17e126f0b2751c21011ae0cccfd096a4f7f56db
Denis <volchekdenisminsk@gmail.com>
2018-08-14 19:08:49 +0300

    Feature/add webpacker (#11)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  # That's all there is:
+  prepend_view_path Rails.root.join("frontend")
 end
--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,13 @@ module Minskrb
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-
+    config.generators do |g|
+      g.test_framework  false
+      g.stylesheets     false
+      g.javascripts     false
+      g.helper          false
+      g.channel         assets: false
+    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
