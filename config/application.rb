10537b555422c38a5a4d6304776a38c1912c7c49
Denis <volchekdenisminsk@gmail.com>
2018-08-14 13:24:14 +0300

    Add form component.Fix CodeFactor.

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,6 @@ module Minskrb
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-    
     config.generators do |g|
       g.test_framework  false
       g.stylesheets     false
--- frontend/packs/application.js
+++ frontend/packs/application.js
@@ -7,4 +7,5 @@
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
 import "init";
-import "components/page/page";
+import "components/home/home";
+import "components/form/form";
