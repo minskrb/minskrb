9faaa22b4c4511e3688b2cdc5ac008c4dcc84df2
ZhenyaZhak <zhenia.zhak@gmail.com>
2018-08-15 18:01:09 +0300

    Some changes in order

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,10 +1,10 @@
 class Event < ApplicationRecord
+  has_many :items, dependent: :destroy
+  accepts_nested_attributes_for :items, allow_destroy: true
+
   validates :title, :place_title, :description, :start_at, :end_at, presence: true
   validates :title, length: { in: 6..20 }
   validates :place_title, length: { maximum: 30 }
   validates :description, length: { minimum: 30 }
   validates_with StartEndTimeValidator
-
-  has_many :items, dependent: :destroy
-  accepts_nested_attributes_for :items, allow_destroy: true
 end
--- app/models/event/item.rb
+++ app/models/event/item.rb
@@ -1,10 +1,11 @@
 class Event::Item < ApplicationRecord
-  enum item_type: [ :talk, :coffee_break, :registration, :after_party, :lunch ]
+  belongs_to :event
+
   validates :title, :item_type, :start_at, :end_at, presence: true
   validates :title, length: { in: 10..100 }
   validates :description, length: { maximum: 200 }
   validates :speaker_name, length: { maximum: 50 }
   validates_with StartEndTimeValidator
 
-  belongs_to :event
+  enum item_type: [ :talk, :coffee_break, :registration, :after_party, :lunch ]
 end
