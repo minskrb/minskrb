700fe28ed52e86e564f14c152ea5a5e72567bb6f
shamritskiy3468 <shamritskiy3468@gmail.com>
2018-08-15 10:17:14 +0300

    Add validations to model Event::MediaPartner

--- Gemfile
+++ Gemfile
@@ -44,6 +44,8 @@ gem 'webpacker', '~> 3.5'
 
 gem 'aws-sdk-s3'
 
+gem 'file_validators'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- app/models/event/media_partner.rb
+++ app/models/event/media_partner.rb
@@ -1,4 +1,7 @@
 class Event::MediaPartner < ApplicationRecord
 	has_one_attached :logo
 	belongs_to :event
+
+	validates :title, :link, presence: true, uniqueness: true, length: { maximum: 128 }
+	validates :logo, file_size: { less_than: 1.megabyte }, file_content_type: { allow: /^image\/.*/ }
 end
