f4fa8235af3502eb7b76ca92e02cd23fa8161b96
Denis <volchekdenisminsk@gmail.com>
2018-08-16 15:45:04 +0300

    Compile main page 3 blocks adaptive.Return to js from jquery.

--- frontend/packs/application.js
+++ frontend/packs/application.js
@@ -6,6 +6,7 @@
 //
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
+
 import "init";
 import "components/form/form";
 import "components/header/header";
