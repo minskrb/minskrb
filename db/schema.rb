c3e9f9fbf1a84b1e04da11d21893c47edae43ca4
ZhenyaZhak <zhenia.zhak@gmail.com>
2018-08-16 13:28:41 +0300

    Resolve merge conflict

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,6 @@
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
 # It's strongly recommended that you check this file into your version control system.
-
 ActiveRecord::Schema.define(version: 2018_08_16_064337) do
 
   # These are extensions that must be enabled in order to support this database
