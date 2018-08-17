688971ef54643ff5cc1dc43b1021d2d27a7cdfb5
ZhenyaZhak <zhenia.zhak@gmail.com>
2018-08-17 13:48:28 +0300

    Add Video dashboard and connect Video and Event dashboards

---
+++ app/controllers/admin/videos_controller.rb
@@ -0,0 +1,21 @@
+module Admin
+  class VideosController < Admin::ApplicationController
+    # To customize the behavior of this controller,
+    # you can overwrite any of the RESTful actions. For example:
+    #
+    # def index
+    #   super
+    #   @resources = Video.
+    #     page(params[:page]).
+    #     per(10)
+    # end
+
+    # Define a custom finder by overriding the `find_resource` method:
+    # def find_resource(param)
+    #   Video.find_by!(slug: param)
+    # end
+
+    # See https://administrate-prototype.herokuapp.com/customizing_controller_actions
+    # for more information
+  end
+end
--- app/models/event.rb
+++ app/models/event.rb
@@ -4,6 +4,7 @@ class Event < ApplicationRecord
   has_many :event_media_partners, dependent: :destroy
   accepts_nested_attributes_for :event_media_partners, allow_destroy: true
   has_many :videos, as: :videoable
+  accepts_nested_attributes_for :videos, allow_destroy: true
 
   validates :title, :place_title, :description, :start_at, :end_at, presence: true
   validates :title, length: { in: 6..20 }
