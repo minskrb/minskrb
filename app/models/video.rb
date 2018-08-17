965f4f67015a8e0a938389313b538ce124aeaa7d
ZhenyaZhak <zhenia.zhak@gmail.com>
2018-08-17 09:34:50 +0300

    Add validation and test that Video must be youtube link

--- app/models/video.rb
+++ app/models/video.rb
@@ -2,4 +2,5 @@ class Video < ApplicationRecord
   belongs_to :videoable, polymorphic: true
 
   validates :youtube_link, presence: true, uniqueness: true, length: { maximum: 128 }
+  validates :youtube_link, format: { with: /\^(http(s)?:\/\/)?((w){3}.)?youtu(be|.be)?(\.com)?\/.+/, message: I18n.t('.error_in_validation_youtube_link') }
 end
