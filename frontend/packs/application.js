e2126fa03542bbe7f40f009ccf5d7e730f553f07
Denis <volchekdenisminsk@gmail.com>
2018-08-18 15:46:05 +0300

    Feature/frontend main page (#24)
    * Remove home component.
    Add header component.
    Add logo.svg
    
    * Adaptive header.
    
    * Add footer
    
    * Compile main page 3 blocks adaptive.Return to js from jquery.
    
    * Add friends block
    
    * Add about section
    
    * Add where section.And codefactore issues.
    
    * Add media partners
    
    * Add speakers
    
    * [CodeFactor] Apply fixes to commit c799de3
    
    * [CodeFactor] Apply fixes to commit bb81bb4
    
    * add jquery w/ eslint config, refactor header component, use components in views
    
    * [CodeFactor] Apply fixes
    
    * Add shedule. initialize About_event
    
    * Event section
    
    * Change index.html
    
    * Shedule section adaptive.  + adaptive fixes in other blocks
    
    * Event content class
    
    * finish event block
    
    * Add main block.Need futere change
    
    * [CodeFactor] Apply fixes to commit aa09860
    
    * [CodeFactor] Apply fixes to commit b565ff9

--- frontend/packs/application.js
+++ frontend/packs/application.js
@@ -6,6 +6,18 @@
 //
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
+
 import "init";
-import "components/home/home";
+import "components/main/main";
+import "components/header/header";
+import "components/event/event";
+import "components/shedule/shedule";
+import "components/speakers/speakers";
+import "components/media/media";
+import "components/where/where";
+import "components/about/about";
+import "components/friends/friends";
 import "components/form/form";
+import "components/footer/footer";
+
+window.$ = jQuery;
