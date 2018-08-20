ca0ee595391554fabc95549bdc11429dd51d544c
Denis <volchekdenisminsk@gmail.com>
2018-08-20 11:10:42 +0300

    Add scroll to event. Hover btn cursor pointer

--- frontend/components/main/main.js
+++ frontend/components/main/main.js
@@ -1,1 +1,9 @@
 import "./main.scss";
+
+$(document).ready(() => {
+  $("#to_event").on("click", function(event) {
+    event.preventDefault();
+    const id = $(this).attr("href");
+    $("body,html").animate({ scrollTop: $(id).offset().top }, 1500);
+  });
+});
