98a28576f6db561d712213932b7681c68267b6f4
ZhenyaZhak <zhenia.zhak@gmail.com>
2018-08-23 11:21:52 +0300

    Add some tests to homepage functional

---
+++ spec/features/homepage_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+RSpec.describe 'Homepage:', type: :system do
+  let!(:event) { create(:event, :with_items, :with_media_partners) }
+
+  before :each do
+    visit '/events'
+  end
+
+  context 'on the user homepage' do
+    describe 'when event exists' do
+      it 'sees the event title' do
+        expect(page).to have_content event.title
+      end
+
+      it 'sees the event place title' do
+        expect(page).to have_content event.place_title
+      end
+
+      it 'sees the event date' do
+        expect(page).to have_content event.start_at.strftime("%d %b %H-%M")
+      end
+
+      it 'sees the event description' do
+        expect(page).to have_content event.description
+      end
+
+      it 'sees the event shedule' do
+        expect(page).to have_content event.event_items.first.title
+      end
+    end
+  end
+end
