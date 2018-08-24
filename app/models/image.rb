6d9926eb88f02e30fa58a378252d330b65cfd5ca
ZhenyaZhak <zhenia.zhak@gmail.com>
2018-08-24 09:09:40 +0300

    Add validation to Image model

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,4 +1,7 @@
 class Image < ApplicationRecord
   has_one_attached :image
   belongs_to :imageable, polymorphic: true
+
+  validates :image, file_size: { less_than_or_equal_to: 1.megabyte },
+                   file_content_type: { allow: ['image/jpeg', 'image/png'] }
 end
