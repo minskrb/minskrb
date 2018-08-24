5a0d510328646d5b30059d4477c566e13037bd54
Nick Pridorozhko <nick.pridorozhko@gmail.com>
2018-08-24 17:59:51 +0300

    fix validation and tests for attachments

--- app/models/event_media_partner.rb
+++ app/models/event_media_partner.rb
@@ -4,5 +4,6 @@ class EventMediaPartner < ApplicationRecord
 
   validates :title, :link, presence: true, uniqueness: true, length: { maximum: 128 }
   validates :logo, file_size: { less_than_or_equal_to: 1.megabyte },
-            file_content_type: { allow: ['image/jpeg', 'image/png'] }, allow_nil: true
+            file_content_type: { allow: ['image/jpeg', 'image/png'] },
+            if: Proc.new { |object| object.logo_attachment.present? }
 end
