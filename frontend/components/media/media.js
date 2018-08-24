a5cd99c77e2abdc1480e413b5abc9079d589becb
Denis <volchekdenisminsk@gmail.com>
2018-08-24 17:39:52 +0300

    Add scrolling to contact_us form

--- frontend/components/media/media.js
+++ frontend/components/media/media.js
@@ -1,1 +1,9 @@
 import "./media.scss";
+
+$(document).ready(() => {
+  $(".to_contact").on("click", function(event) {
+    event.preventDefault();
+    const link = $(this).attr("href");
+    $("body,html").animate({ scrollTop: $(link).offset().top }, 1500);
+  });
+});
