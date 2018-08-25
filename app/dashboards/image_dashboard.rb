318b6bfbac1fa87ea440daff66186030f05bd809
ZhenyaZhak <zhenia.zhak@gmail.com>
2018-08-25 16:11:49 +0300

    Add condition to Image#photo validation

--- app/dashboards/image_dashboard.rb
+++ app/dashboards/image_dashboard.rb
@@ -22,6 +22,7 @@ class ImageDashboard < Administrate::BaseDashboard
   # Feel free to add, remove, or rearrange items.
   COLLECTION_ATTRIBUTES = [
     :imageable,
+    :photo,
     :id,
   ].freeze
 
--- app/models/image.rb
+++ app/models/image.rb
@@ -3,5 +3,6 @@ class Image < ApplicationRecord
   belongs_to :imageable, polymorphic: true
 
   validates :photo, file_size: { less_than_or_equal_to: 1.megabyte },
-                   file_content_type: { allow: ['image/jpeg', 'image/png'] }
+            file_content_type: { allow: ['image/jpeg', 'image/png'] },
+            if: Proc.new { |object| object.photo_attachment.present? }
 end
