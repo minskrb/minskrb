8ee5d85b8d4b487da6e87ae869a92b645362fac2
Denis <volchekdenisminsk@gmail.com>
2018-08-26 19:53:38 +0300

    Add active status to links according with curent url

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,5 +4,10 @@ module ApplicationHelper
     render("components/#{name}/#{component_name}", locals, &block)
   end
 
+  def current_page?(path)
+    return 'active' if request.path == path
+  end
+
   alias c component
+  alias cp current_page?
 end
