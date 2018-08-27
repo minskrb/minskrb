2c780f1caa87264dcc085784d3b8f5f6e030bc69
ZhenyaZhak <zhenia.zhak@gmail.com>
2018-08-27 18:58:25 +0300

    Some change in syntax

--- app/models/image.rb
+++ app/models/image.rb
@@ -4,5 +4,5 @@ class Image < ApplicationRecord
 
   validates :photo, file_size: { less_than_or_equal_to: 1.megabyte },
             file_content_type: { allow: ['image/jpeg', 'image/png'] },
-            if: Proc.new { |object| object.photo_attachment.present? }
+            if: proc { |object| object.photo_attachment.present? }
 end
