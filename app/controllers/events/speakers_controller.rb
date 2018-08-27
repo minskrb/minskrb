9d75fc64ba61835494938853ccf5666ec8da7ef7
Denis <volchekdenisminsk@gmail.com>
2018-08-27 20:08:46 +0300

    Change scopes naming

--- app/controllers/events/speakers_controller.rb
+++ app/controllers/events/speakers_controller.rb
@@ -2,7 +2,7 @@ module Events
   class SpeakersController < ApplicationController
     def index
       @event = Event.find(params[:event_id])
-      @speakers = @event.event_items.show_speakers
+      @speakers = @event.event_items.talks
     end
   end
 end
