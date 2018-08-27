57e2764719acb0a1e8c56960b932476f55f1ae8f
Nick Pridorozhko <nick.pridorozhko@gmail.com>
2018-08-28 02:34:43 +0300

    refactor sending mails a bit

--- app/controllers/contact_us_controller.rb
+++ app/controllers/contact_us_controller.rb
@@ -2,17 +2,22 @@ class ContactUsController < ApplicationController
   include SendGrid
 
   def create
-    mail = prepare_mail
     sg = SendGrid::API.new(api_key: Rails.application.credentials.sendgrid[:api_key])
-    sg.client.mail._('send').post(request_body: mail.to_json)
+    sg.client.mail._('send').post(request_body: contact_mail.to_json)
   end
 
   private
 
-  def prepare_mail
-    Mail.new(Email.new(email: params[:email]),
-             'Minsk.rb contact us form',
-             Email.new(email: 'zhenia.zhak@gmail.com'),
-             Content.new(type: 'text/plain', value: "#{params[:name]}:\n#{params[:message]}"))
+  def contact_mail
+    mail = Mail.new
+    personalization = Personalization.new
+    AdminUser.pluck(:email).map { |email| personalization.add_to(Email.new(email: email)) }
+    mail.add_personalization(personalization)
+
+    mail.from = Email.new(email: params[:email].html_safe)
+    mail.reply_to = Email.new(email: params[:email].html_safe)
+    mail.subject = 'Minsk.rb Contact Form'
+    mail.add_content(Content.new(type: 'text/html', value: "<html><body><b>#{params[:name].html_safe}:</b><br/>#{params[:message].html_safe}</body></html>"))
+    mail
   end
 end
