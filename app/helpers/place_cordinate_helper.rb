8a82ca1897e81f051ac6e358900b7463a8a2e894
ZhenyaZhak <zhenia.zhak@gmail.com>
2018-08-28 11:25:20 +0300

    Some changes in view place cordinate

---
+++ app/helpers/place_cordinate_helper.rb
@@ -0,0 +1,13 @@
+module PlaceCordinateHelper
+  GOOGLE_API_URL = 'https://www.google.com/maps/embed/v1/place?key=AIzaSyDc4bU611NyBZ_2PJxohPbLTpQWk7UUiaw&q='.freeze
+
+  def google_api_cordinate(cordinate)
+    URI::encode(GOOGLE_API_URL + valid_cordinate(cordinate))
+  end
+
+  private
+
+  def valid_cordinate(cordinate)
+    Geocoder.search(cordinate).empty? ? 'Belarus' : cordinate
+  end
+end
