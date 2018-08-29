6d9c2f3f73b932d4c37e60a25300eeed2a715a5d
ZhenyaZhak <zhenia.zhak@gmail.com>
2018-08-29 12:59:35 +0300

    Fix bug in showing nil event

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,6 @@
 class EventsController < ApplicationController
   def index
     @event = Event.upcoming
-    @schedule = @event.event_items.schedule
+    @schedule = @event&.event_items&.schedule
   end
 end
