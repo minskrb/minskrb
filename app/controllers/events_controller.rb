4266598e1eff26b0898805a887c1eb87191f4566
Denis <volchekdenisminsk@gmail.com>
2018-08-30 09:58:19 +0300

    Add @seakers to EventsController

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,5 +2,6 @@ class EventsController < ApplicationController
   def index
     @event = Event.upcoming
     @schedule = EventItem.where(event: @event)
+    @speakers = @event.event_items.talks
   end
 end
