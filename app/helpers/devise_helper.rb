9758f61bb8daad06291687543338c150692520d0
ZhenyaZhak <zhenia.zhak@gmail.com>
2018-08-31 11:15:22 +0300

    Add Error message and style for this

---
+++ app/helpers/devise_helper.rb
@@ -0,0 +1,17 @@
+module DeviseHelper
+  def devise_error_messages!
+    return '' if resource.errors.empty?
+
+    messages = resource.errors.full_messages.map { |msg| content_tag(:li, msg) }.join
+    html = <<-HTML
+    <div class="alert alert-danger alert-dismissible fade show" role="alert">
+      <strong>
+       #{pluralize(resource.errors.count, "error")} must be fixed
+      </strong>
+      #{messages}
+    </div>
+    HTML
+
+    html.html_safe
+  end
+end
